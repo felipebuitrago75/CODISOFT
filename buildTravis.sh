2a5361dd76f5b046bbfd802aa0e229f69dbc67b9
Andres Felipe Buitrago Feria <felipebuitrago75@gmail.com>
2020-09-22 14:03:05 -0500

    Se actualiza CI

---
+++ buildTravis.sh
@@ -0,0 +1,32 @@
+CWD=$(pwd) #Obtiene el directorio de ejecución
+
+./cleanDocker.sh
+
+#Se Actualiza el repositorio
+git pull
+
+#Se construye el backend
+cd fxmanager-management
+./gradlew clean 
+./gradlew buildDocker -x test
+
+cd $CWD
+docker tag co.com.smartcode/fxmanager:0.0.1-SNAPSHOT  fxmanager
+docker rmi -f co.com.smartcode/fxmanager:0.0.1-SNAPSHOT
+docker push felipebuitrago75/fxmanager:fxmanager
+
+#Se construye el frontend
+cd fxmanager-ng/docker
+./ng-build
+
+cd $CWD
+
+docker push felipebuitrago75/fxmanager:fxmanager
+
+#Se construye la base de datos
+cd config/database
+./build-docker
+
+cd $CWD
+
+docker push felipebuitrago75/fxmanager:fxmanager-db
